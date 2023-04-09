f218bf61cb209954ba7f64a5c50da7acd198e176
kubraozyakisir <kbrozyksr@gmail.com>
2023-04-10 02:11:39 +0300

    Created template and models

--- E-Commerce-Basket/src/app/components/home/home.component.ts
+++ E-Commerce-Basket/src/app/components/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductModel } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  products:ProductModel[]=[
+
+  ]
+
+    ngOnInit(): void {
+      this.setProducts();
+    }
+    setProducts(){
+      for(let i=0;i<100;i++){
+          let product = new ProductModel();
+          product.id=i;
+          product.name=`Product${i}`;
+          product.unitPrice=80;
+          product.stock=100+i*10;
+          this.products.push(product);
+      }
+    }
+
+
+
 
 }
